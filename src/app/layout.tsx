17d1232793174b8f05d5e09ae14d9c4cd6711ebc
Pontus Abrahamsson <pontus@lostisland.co>
2024-08-25 13:03:40 +0200

    Add ping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,11 @@ export default function RootLayout({
                   </clipPath>
                 </defs>
               </svg>
-              <span>Live on Hacker News</span>
+              <span>Vote on Hacker News</span>
+              <span className="relative ml-auto flex h-2 w-2">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 bg-green-400" />
+                <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500" />
+              </span>
             </Button>
           </a>
         </div>
