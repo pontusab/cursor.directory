51778d94a2a9929f6be6478a54700fb34c572d40
Pontus Abrahamsson <pontus@lostisland.co>
2024-08-25 21:18:24 +0200

    Video support

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
       </div>
 
       <main className="flex-1 p-6 pt-16">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 sm:grid-cols-1">
           {videos.map((video) => (
             <Card key={video.url}>
               <CardHeader>
