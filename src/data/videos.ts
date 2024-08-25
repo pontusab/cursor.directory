693f27e276ec1546062b9e3cc5064589d0637927
Pontus Abrahamsson <pontus@lostisland.co>
2024-08-25 21:13:23 +0200

    Add videos wip

---
+++ src/data/videos.ts
@@ -0,0 +1,34 @@
+export const videos = [
+  {
+    title: "Coding with Cursor: Session 1",
+    description: "Coding with Cursor: Session 1",
+    url: "https://www.youtube.com/watch?v=1CC88QGQiEA",
+    author: {
+      name: "Sahil Lavingia",
+      image:
+        "https://yt3.ggpht.com/7Hvm8iHnumiLr8aWtftr6rNckmhqt7FvhbxmUMD9eB55v_BSDmCPiFtCVRgR2JowNyz6al4Ohg=s88-c-k-c0x00ffffff-no-rj",
+    },
+  },
+  {
+    title: "How I code 159% Faster using AI (Cursor + Sonnet 3.5)",
+    description:
+      "Cursor is a powerful new AI-powered code editor that makes coding much faster with the help of AI. In this video I will show you the fastest way to code with the best AI coding tools: Cursor and Claude Sonnet 3.5.",
+    url: "https://www.youtube.com/watch?v=yk9lXobJ95E",
+    author: {
+      name: "Volo",
+      image:
+        "https://yt3.ggpht.com/5SaI-Z9lEUBFO0Uv0wZK9olaKiLBmqyDlELKQywZQIdiBh_cuJMJqrvk2np3OFUMNalDrTdO=s88-c-k-c0x00ffffff-no-rj",
+    },
+  },
+  {
+    title: "I Finally Tried The AI-Powered VS Code Killer | Cursor IDE Review",
+    description:
+      "I Finally Tried The AI-Powered VS Code Killer | Cursor IDE Review",
+    url: "https://www.youtube.com/watch?v=u3wPImWBz7c",
+    author: {
+      name: "Your Average Tech Bro",
+      image:
+        "https://yt3.ggpht.com/-5pyvUOmvkobQLYDV39VhjNU4Fp4Z178V3_pHuxrokzwinC-CFo1omaY7Ra5-A_N7gLynPS3=s88-c-k-c0x00ffffff-no-rj",
+    },
+  },
+];
