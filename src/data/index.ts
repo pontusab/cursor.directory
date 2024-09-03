37a00dfb6388f47a040a430d6d977ac2210786cb
Bran18 <andreyfdez18@gmail.com>
2024-09-03 14:00:11 -0600

    feat/webDevelopment-rules

--- src/data/index.ts
+++ src/data/index.ts
@@ -26,6 +26,7 @@ import { swiftuiRules } from "./rules/swift";
 import { tauriRules } from "./rules/tauri";
 import { unityCSharpRules } from "./rules/unity-c-sharp";
 import { vueTsRules } from "./rules/vue";
+import { webDevelopment } from './rules/web-development';
 
 export const rules = [
   ...astroRules,
@@ -56,6 +57,7 @@ export const rules = [
   ...svelteKit5Rules,
   ...gatsbyRules,
   ...solidityRules,
+  ...webDevelopment
 ];
 
 export function getSections() {
