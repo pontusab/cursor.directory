92777d9c581554b6571a4b8de3ae82af9d294041
Pontus Abrahamsson <pontus@lostisland.co>
2024-10-12 14:52:34 +0200

    Fix build

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,57 +1,56 @@
 import { elixirRules } from "@/data/rules/elixir";
+import { ALRules } from "./rules/al";
 import { angularRules } from "./rules/angular";
-import { autohotkeyRules } from "./rules/autohotkey";
 import { astroRules } from "./rules/astro";
+import { autohotkeyRules } from "./rules/autohotkey";
+import { blazorRules } from "./rules/blazor";
+import { bootstrapRules } from "./rules/bootstrap";
 import { cRules } from "./rules/c";
+import { convexRules } from "./rules/convex";
 import { dataAnalystRules } from "./rules/data-analyst";
 import { deepLearningRules } from "./rules/deep-learning";
 import { djangoRules } from "./rules/django";
 import { dotnetRules } from "./rules/dotnet";
 import { expoReactNativeRules } from "./rules/expo";
 import { fastapiRules } from "./rules/fastapi";
+import { fastifyRules } from "./rules/fastify";
 import { flaskRules } from "./rules/flask";
 import { flutterRules } from "./rules/flutter";
 import { frontEndRules } from "./rules/front-end";
 import { gatsbyRules } from "./rules/gastby";
 import { goRules } from "./rules/go";
 import { htmlAndCssRules } from "./rules/htmlandcss";
+import { htmxRules } from "./rules/htmx";
+import { ionicRules } from "./rules/ionic";
 import { javaRules } from "./rules/java";
+import { jaxRules } from "./rules/jax";
 import { juliaRules } from "./rules/julia";
 import { laravelRules } from "./rules/laravel";
 import { metaPromptRules } from "./rules/meta-prompt";
+import { monorepoTamagui } from "./rules/monorepo-tamagui";
 import { nestjsRules } from "./rules/nestjs";
 import { nextjsRules } from "./rules/nextjs";
 import { nuxtJsRules } from "./rules/nuxtjs";
+import { pixiJsRules } from "./rules/pixijs";
 import { pythonRules } from "./rules/python";
 import { railsRules } from "./rules/rails";
 import { reactNativeRules } from "./rules/react-native";
+import { remixRules } from "./rules/remix";
 import { rustRules } from "./rules/rust";
+import { salesforceRules } from "./rules/salesforce";
 import { solanaRules } from "./rules/solana";
 import { solidityRules } from "./rules/solidity";
 import { svelteRules } from "./rules/svelte";
 import { svelteKitRules } from "./rules/sveltekit";
 import { swiftuiRules } from "./rules/swift";
 import { tauriRules } from "./rules/tauri";
+import { technicalTutorialsRules } from "./rules/technical-tutorials";
+import { uiuxRules } from "./rules/uiux-design";
 import { unityCSharpRules } from "./rules/unity-c-sharp";
 import { vueTsRules } from "./rules/vue";
 import { webDevelopmentRules } from "./rules/web-development";
-import { uiuxRules } from "./rules/uiux-design";
-import { ionicRules } from "./rules/ionic";
-import { fastifyRules } from "./rules/fastify";
-import { pixiJsRules } from "./rules/pixijs";
-import { technicalTutorialsRules } from "./rules/technical-tutorials";
-import { monorepoTamagui } from "./rules/monorepo-tamagui";
-import { convexRules } from "./rules/convex";
-import { remixRules } from "./rules/remix";
-import { salesforceRules } from "./rules/salesforce";
 import { wordpressRules } from "./rules/wordpress";
 import { wordpressWoocommerce } from "./rules/wordpress-woocommerce";
-import { blazorRules } from "./rules/blazor";
-import { htmxRules } from "./rules/htmx";
-import { bootstrapRules } from "./rules/bootstrap";
-import { jaxRules } from "./rules/jax";
-import { ALRules } from "./rules/AL";
-
 
 interface Rule {
   title: string;
@@ -69,58 +68,60 @@ interface Author {
 }
 
 export const rules: Rule[] = [
-  ...convexRules,
-  ...jaxRules,
-  ...astroRules,
-  ...nextjsRules,
-  ...swiftuiRules,
-  ...laravelRules,
-  ...railsRules,
-  ...fastapiRules,
-  ...flaskRules,
-  ...frontEndRules,
-  ...djangoRules,
-  ...cRules,
-  ...vueTsRules,
-  ...metaPromptRules,
-  ...expoReactNativeRules,
-  ...reactNativeRules,
-  ...javaRules,
-  ...juliaRules,
-  ...dataAnalystRules,
-  ...goRules,
-  ...pythonRules,
-  ...elixirRules,
-  ...deepLearningRules,
-  ...tauriRules,
-  ...nestjsRules,
-  ...unityCSharpRules,
-  ...flutterRules,
+  ...ALRules,
   ...angularRules,
+  ...astroRules,
+  ...autohotkeyRules,
+  ...blazorRules,
+  ...bootstrapRules,
+  ...cRules,
+  ...convexRules,
+  ...dataAnalystRules,
+  ...deepLearningRules,
+  ...djangoRules,
   ...dotnetRules,
-  ...rustRules,
-  ...svelteKitRules,
-  ...gatsbyRules,
-  ...solidityRules,
-  ...webDevelopmentRules,
-  ...solanaRules,
-  ...svelteRules,
-  ...htmlAndCssRules,
-  ...nuxtJsRules,
-  ...uiuxRules,
-  ...ionicRules,
+  ...elixirRules,
+  ...expoReactNativeRules,
+  ...fastapiRules,
   ...fastifyRules,
-  ...pixiJsRules,
-  ...technicalTutorialsRules,
+  ...flaskRules,
+  ...flutterRules,
+  ...frontEndRules,
+  ...gatsbyRules,
+  ...goRules,
+  ...htmlAndCssRules,
+  ...htmxRules,
+  ...ionicRules,
+  ...javaRules,
+  ...jaxRules,
+  ...juliaRules,
+  ...laravelRules,
+  ...metaPromptRules,
   ...monorepoTamagui,
+  ...nestjsRules,
+  ...nextjsRules,
+  ...nuxtJsRules,
+  ...pixiJsRules,
+  ...pythonRules,
+  ...railsRules,
+  ...reactNativeRules,
   ...remixRules,
+  ...rustRules,
   ...salesforceRules,
+  ...solanaRules,
+  ...solidityRules,
+  ...svelteRules,
+  ...svelteKitRules,
+  ...swiftuiRules,
+  ...tauriRules,
+  ...technicalTutorialsRules,
+  ...uiuxRules,
+  ...unityCSharpRules,
+  ...vueTsRules,
+  ...webDevelopmentRules,
   ...wordpressRules,
   ...wordpressWoocommerce,
-  ...htmxRules,
-  ...bootstrapRules,
 ];
-
 
 export function getSections() {
   const categories = Array.from(new Set(rules.flatMap((rule) => rule.tags)));
