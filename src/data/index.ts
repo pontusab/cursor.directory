fd4d8035c12c7f032a66105243f3ac79b0486fb8
Allen <allenmxh.cn@gmail.com>
2024-12-06 17:31:47 +0800

    Add servicenow rules

--- src/data/index.ts
+++ src/data/index.ts
@@ -57,6 +57,8 @@ import { luaRules } from "./rules/lua";
 import { androidRules } from "./rules/android";
 import { ghostTailwindcssRules } from "./rules/ghost-tailwindcss";
 import { devopsRules } from "./rules/devops-backend";
+import { servicenowRules } from "./rules/servicenow";
+
 
 export interface Rule {
   title: string;
@@ -133,6 +135,7 @@ export const rules: Rule[] = [
   ...androidRules,
   ...ghostTailwindcssRules,
   ...devopsRules,
+  ...servicenowRules
 ].map(
   (rule): Rule => ({
     ...rule,
