6d8697d771b727120d249a26fc7db7b82e1bec27
Pontus Abrahamsson <pontus@lostisland.co>
2025-02-13 14:57:09 +0100

    Generator

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -2,7 +2,7 @@ import { Generate } from "@/components/generate";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen w-full px-2 sm:px-6">
+    <div className="flex items-center justify-center min-h-screen w-full px-2 -mt-16 sm:mt-0">
       <div className="w-full max-w-3xl">
         <Generate />
       </div>
